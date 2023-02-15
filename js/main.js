ba6c37e322001e4503eadf1dcc3a07fac976427d
Rasmus-Brandt-NTI-Johanneberg <119052518+Rasmus-Brandt-NTI-Johanneberg@users.noreply.github.com>
2023-02-15 17:32:12 +0100

    Fixat menu för mobil

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-document.querySelector(".fade-layer").addEventListener("click", showMenu);
-
-document.querySelector(".menu_button").addEventListener("click", showMenu);
-
-
-function showMenu(){
-    let menu = document.querySelector("nav.menu");
-    menu.classList.toggle("show");
-
-    let layer = document.querySelector(".fade-layer")
-    layer.classList.toggle("visible")
-}
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".fade_layer").addEventListener("click", showMenu);
+    document.querySelector(".menu_button").addEventListener("click", showMenu);
+  
+    function showMenu(){
+      let menu = document.querySelector("nav.menu");
+      menu.classList.toggle("show");
+  
+      let layer = document.querySelector(".fade_layer")
+      layer.classList.toggle("visible")
+    }
+  });
