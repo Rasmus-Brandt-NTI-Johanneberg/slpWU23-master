7533643e56d481e66118ab1ff32d66f03207cef0
Rasmus-Brandt-NTI-Johanneberg <119052518+Rasmus-Brandt-NTI-Johanneberg@users.noreply.github.com>
2023-03-03 13:46:04 +0100

    Påbörjad services_page

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ document.querySelector(".menu_button").addEventListener("click", showMenu);
 function showMenu(){
   let menu = document.querySelector("nav.menu");
   menu.classList.toggle("show");
-
+  
   let layer = document.querySelector(".fade_layer")
   layer.classList.toggle("visible")
 }
