ce23227a77cf017f4b4936b65c2c2e42d6a13ed2
Rasmus-Brandt-NTI-Johanneberg <119052518+Rasmus-Brandt-NTI-Johanneberg@users.noreply.github.com>
2023-03-17 13:56:12 +0100

    Shop js funkar

--- js/main.js
+++ js/main.js
@@ -3,37 +3,28 @@
 document.querySelector(".fade_layer").addEventListener("click", showMenu);
 document.querySelector(".menu_button").addEventListener("click", showMenu);
 
-function showMenu(){
+function showMenu() {
   let menu = document.querySelector("nav.menu");
   menu.classList.toggle("show");
-  
+
   let layer = document.querySelector(".fade_layer")
   layer.classList.toggle("visible")
 }
 
 
-// document.querySelectorAll(".products_button").forEach(divs => {
-//   divs.addEventListener("click", function(event) {
-//     let clickedElementClass = event.target.className;
-//     let dropdown = document.querySelectorAll(".products_section",{clickedElementClass});
-//     dropdown.forEach(d => d.classList.toggle("show_products"));
 
-//   }) 
-// })
+// Shop sections
+const productButtons = document.querySelectorAll('.products_button');
 
-document.querySelectorAll(".products_button").forEach(div => {
-  div.addEventListener("click", function(event) {
-    let clickedElementClass = event.target.className;
-    let dropdown = document.querySelector(".products_section",{clickedElementClass});
-    dropdown.classList.toggle("show_products");
-  })
-})
+productButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const productSection = button.nextElementSibling;
+    dropdownProducts(button, productSection);
+  });
+});
 
+function dropdownProducts(button, productSection){
+  productSection.classList.toggle("show_products");
+  button.classList.toggle("wide", productSection.classList.contains("show_products"));
+}
 
-// function dropdownProducts(){
-//   let dropdown = document.querySelector(".products_section#protein_section")
-//   dropdown.classList.toggle("show_products")
-// }
-  
-
-  //this.nextElementSibling.classlist.toggle
