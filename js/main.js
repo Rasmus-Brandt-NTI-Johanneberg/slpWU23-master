80777faa75be82d6916cbf7fe9e2c952aeb9ad7a
Rasmus-Brandt-NTI-Johanneberg <119052518+Rasmus-Brandt-NTI-Johanneberg@users.noreply.github.com>
2023-04-02 16:07:10 +0200

    Hemsida typ klar

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 // Nav Menu Mobile
-
 document.querySelector(".fade_layer").addEventListener("click", showMenu);
 document.querySelector(".menu_button").addEventListener("click", showMenu);
 
 function showMenu() {
-  let menu = document.querySelector("nav.menu");
-  menu.classList.toggle("show");
+    let menu = document.querySelector("nav.menu");
+    menu.classList.toggle("show");
 
-  let layer = document.querySelector(".fade_layer")
-  layer.classList.toggle("visible")
+    let layer = document.querySelector(".fade_layer")
+    layer.classList.toggle("visible")
 }
 
 // Header Universal
@@ -17,21 +16,12 @@ const navText = document.querySelectorAll('.nav_link');
 const icons = document.querySelector('.nav_icons');
 
 window.addEventListener('scroll', () => {
-  if (window.pageYOffset <= 20) {
-    header.classList.remove('header_universal--transparent');
-    navText.forEach((element) => {
-      element.classList.remove('header_white_text');
-    });
-    icons.classList.remove('header_white_text');
-  } else {
-    header.classList.add('header_universal--transparent');
-    navText.forEach((element) => {
-      element.classList.add('header_white_text');
-    });
-    icons.classList.add('header_white_text');
-  }
+    if (window.pageYOffset <= 20) {
+        header.classList.remove('header_universal--transparent');
+    } else {
+        header.classList.add('header_universal--transparent');
+    }
 });
-
 
 
 
@@ -40,28 +30,29 @@ window.addEventListener('scroll', () => {
 const productButtons = document.querySelectorAll('.products_button');
 
 productButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    dropdownProducts(button);
-  });
+    button.addEventListener('click', () => {
+        dropdownProducts(button);
+    });
 });
 
-function dropdownProducts(button){
-  const productSection = button.nextElementSibling;
-  const isShowing = productSection.classList.contains("show_products");
+function dropdownProducts(button) {
+    const productSection = button.nextElementSibling;
+    const isShowing = productSection.classList.contains("show_products");
 
-  if (!isShowing) {
-    productSection.style.animation = "show_section 1.6s ease-in-out forwards";
-  }
+    if (!isShowing) {
+        productSection.style.animation = "show_section 1.6s ease-in-out forwards";
+    }
 
-  productSection.classList.toggle("show_products");
+    productSection.classList.toggle("show_products");
 
-  button.classList.toggle("wide", productSection.classList.contains("show_products"));
+    button.classList.toggle("wide", productSection.classList.contains("show_products"));
 
-  if (isShowing) {
-    productSection.style.animation = "hide_section 0.8s ease-in-out forwards";
-    productSection.addEventListener("animationend", () => {
-      productSection.style.animation = "none";
-    }, { once: true });
-  }
+    if (isShowing) {
+        productSection.style.animation = "hide_section 0.8s ease-in-out forwards";
+        productSection.addEventListener("animationend", () => {
+            productSection.style.animation = "none";
+        }, {
+            once: true
+        });
+    }
 }
-
